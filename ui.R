a44f71b6ddc1d93e2caf2b1a10b525fe478262ed
Qiime User <qiime@qiime-190-virtual-box.(none)>
2016-01-27 09:01:06 -0500

    Edit number 3

--- ui.R
+++ ui.R
@@ -1,6 +1,8 @@
 library(shiny)
 ui <- fluidPage(
   leafletOutput("myMap", height = 820),
+  absolutePanel(top=10,right=10,
+                sliderInput("range", "Months", min(STI_2$visit.month_n),max(STI_2$visit.month_n),value=range(STI_2$visit.month_n),step=1)),
   p()
 )
 
