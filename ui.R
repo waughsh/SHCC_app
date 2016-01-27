ff3cdf08020a814d34917cf3e648403888d6f665
Qiime User <qiime@qiime-190-virtual-box.(none)>
2016-01-27 13:28:13 -0500

    Added Year Slider (Just numbers not actual labels for years at this time)

--- ui.R
+++ ui.R
@@ -2,9 +2,10 @@ library(shiny)
 ui <- fluidPage(
   leafletOutput("myMap", height = 820),
   absolutePanel(top=10,right=10,
-                sliderInput("range", "Months", min(STI_2$visit.month_n),max(STI_2$visit.month_n),value=range(STI_2$visit.month_n),step=1)),
+                sliderInput("range", "Months", min(STI_2$visit.month_n),max(STI_2$visit.month_n),value=range(STI_2$visit.month_n),step=1),
+                sliderInput("range_2", "Months", min(STI_2$visit.year),max(STI_2$visit.year),value=range(STI_2$visit.year),step=1)),
   fluidRow(
-    column(width = 10, "This is a Alpha Version of this application, all data is curretly housed in a secure server at the Emerging Pathogens Institute")
+    column(width = 10, "This is a ALPHA VERSION APPLICATION, all data is curretly housed in a secure server at the Emerging Pathogens Institute")
   ),
   p()
 )
