0f4eeb62c0f36f1f83c0f61280aa286d8f5a28b0
Qiime User <qiime@qiime-190-virtual-box.(none)>
2016-01-27 13:29:47 -0500

    Changed label to years

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ ui <- fluidPage(
   leafletOutput("myMap", height = 820),
   absolutePanel(top=10,right=10,
                 sliderInput("range", "Months", min(STI_2$visit.month_n),max(STI_2$visit.month_n),value=range(STI_2$visit.month_n),step=1),
-                sliderInput("range_2", "Months", min(STI_2$visit.year),max(STI_2$visit.year),value=range(STI_2$visit.year),step=1)),
+                sliderInput("range_2", "Years", min(STI_2$visit.year),max(STI_2$visit.year),value=range(STI_2$visit.year),step=1)),
   fluidRow(
     column(width = 10, "This is a ALPHA VERSION APPLICATION, all data is curretly housed in a secure server at the Emerging Pathogens Institute")
   ),
