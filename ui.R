9cdbd1918ece234ba16daf2d2a49d23034707b85
Qiime User <qiime@qiime-190-virtual-box.(none)>
2016-01-27 13:12:20 -0500

    Added Disclaimer

--- ui.R
+++ ui.R
@@ -3,6 +3,9 @@ ui <- fluidPage(
   leafletOutput("myMap", height = 820),
   absolutePanel(top=10,right=10,
                 sliderInput("range", "Months", min(STI_2$visit.month_n),max(STI_2$visit.month_n),value=range(STI_2$visit.month_n),step=1)),
+  fluidRow(
+    column(width = 10, "This is a Alpha Version of this application, all data is curretly housed in a secure server at the Emerging Pathogens Institute")
+  ),
   p()
 )
 
